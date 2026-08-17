c0e2f2ada7906feeada0eaef63ab6fd2da86e481
Leenabintmohammed <leenabintmohammed200@gmail.com>
2026-08-18 00:36:09 +0400

    Refactor vite.config.ts to include environment loading for cloudflare
    Load environment variables using loadEnv for configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,37 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-export default defineConfig({
-  resolve: {
-    tsconfigPaths: true,
-  },
-  plugins: [
-    cloudflare({
-      viteEnvironment: {
-        name: "ssr",
-      },
-    }),
-    tailwindcss(),
-    tanstackStart({
-      server: {
-        entry: "server",
-      },
-    }),
-    react(),
-  ],
+  return {
+    define: {
+      "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(
+        env.VITE_SUPABASE_URL
+      ),
+      "import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY": JSON.stringify(
+        env.VITE_SUPABASE_PUBLISHABLE_KEY
+      ),
+    },
+
+    resolve: {
+      tsconfigPaths: true,
+    },
+
+    plugins: [
+      cloudflare({
+        viteEnvironment: {
+          name: "ssr",
+        },
+      }),
+      tailwindcss(),
+      tanstackStart({
+        server: {
+          entry: "server",
+        },
+      }),
+      react(),
+    ],
+  };
 });
